8df00ec4e412a02acdaad9796c966ff09c6207eb
AnnTs <atserkovnikova.qa@gmail.com>
2023-11-05 13:34:14 +0300

    added homework

---
+++ src/test/java/github/CheckJUnit5CodeTest.java
@@ -0,0 +1,43 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CheckJUnit5CodeTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void SuccesfulCheckJUnit5CodeTest() {
+
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-wrapper").shouldHave(text("Welcome to the selenide wiki!"));
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click(); //нажать на результат поиска, тк по Enter не срабатывает переход
+        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+
+    }
+}
